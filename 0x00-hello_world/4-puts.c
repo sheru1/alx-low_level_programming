f0dfe0cc229ea3894ac153ca94d687cf486d3ea4
sheru1 <sampsonak95@gmail.com>
2023-02-16 23:19:36 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * main -program that prints a line
+  * Return: 0 (Success)
+  */
+int main(void)
+{
+puts("/"Programming is like building a multilingual puzzle");
+return (0);
+}
